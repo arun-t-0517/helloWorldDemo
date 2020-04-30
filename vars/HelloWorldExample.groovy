3693f0da810be9d38de761010b694c4bed3fdb0b
arun-t-0517 <64598128+arun-t-0517@users.noreply.github.com>
2020-04-30 13:18:05 -0400

    sending mail test

--- vars/HelloWorldExample.groovy
+++ vars/HelloWorldExample.groovy
@@ -4,7 +4,6 @@ import arun.test.helloworlddemo.model.Constants
 def call(user) {
 
     def helloWorld = new HelloWorld(user);
-    def Constants
     echo "${helloWorld.get_message()}"
     echo Constants.EMAIL_MESSAGE
 }
